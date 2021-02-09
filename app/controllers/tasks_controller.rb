e8f2c40dd8ee1138839679eba61bd7777f2ab6a2
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-02-09 10:00:16 +0900

    コントローラー

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:edit, :update, :show, :destroy]
+
   def index
     @tasks = Task.all
   end
@@ -8,5 +10,43 @@ class TasksController < ApplicationController
   end
 
   def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to  tasks_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to  task_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if user_signed_in? && current_user.id == 1
+      @task.destroy
+      redirect_to  tasks_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:name, :info, :event_date).merge(user_id: current_user.id)
+  end
+
+  def set_task
+   @task = Task.find(params[:id])
   end
 end
